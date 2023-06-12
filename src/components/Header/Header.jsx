2b692cc246ac873993b65129ef68574fcd062990
Henry Caiza <henrypcv1995@gmail.com>
2023-06-12 16:54:44 -0500

    correo y boton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,54 +16,60 @@ function Header() {
                     onClick={() => setMenuOpen(true)}>
                     <img src={Bars} alt="" className='w-6 h-6' />
                 </div>
-            ) : (<ul className='flex flex-row gap-8 text-white mt-3
-            max-sm:fixed max-sm:right-8 max-sm:z-50 max-sm:flex-col max-sm:bg-gray-600 max-sm:p-8'>
-                <li className='cursor-pointer hover:text-primary' >
-                    <Link
-                        activeClass='active'
-                        to='home'
-                        span={true}
-                        smooth={true}
-                        onClick={() => setMenuOpen(false)}
-                    >Inicio</Link>
-                </li>
-                <li className='cursor-pointer hover:text-primary' >
-                    <Link
-                        to='programs'
-                        span={true}
-                        smooth={true}
-                        onClick={() => setMenuOpen(false)}
-                    >Programas
-                    </Link>
-                </li>
-                <li className='cursor-pointer hover:text-primary' >
-                    <Link
-                        to='features'
-                        span={true}
-                        smooth={true}
-                        onClick={() => setMenuOpen(false)}
-                    >Sobre nosotros
-                    </Link>
-                </li>
-                <li className='cursor-pointer hover:text-primary' >
-                    <Link
-                        to='plans'
-                        span={true}
-                        smooth={true}
-                        onClick={() => setMenuOpen(false)}
-                    >Plenes
-                    </Link>
-                </li>
-                <li className='cursor-pointer hover:text-primary' >
-                    <Link
-                        onClick={() => setMenuOpen(false)}
-                        to='testimonials'
-                        span={true}
-                        smooth={true}
-                    >Testimonios</Link>
 
-                </li>
-            </ul>)}
+
+            ) : (
+                <div className='fixed top-0 right-0 w-full h-full z-10 bg-gray-900/50' onClick={() => setMenuOpen(false)}>
+                    <ul className='flex flex-row gap-8 text-white mt-3
+            max-sm:fixed max-sm:right-8 max-sm:z-50 max-sm:flex-col max-sm:bg-gray-600 max-sm:p-8'>
+                        <li className='cursor-pointer hover:text-primary' >
+                            <Link
+                                activeClass='active'
+                                to='home'
+                                span={true}
+                                smooth={true}
+                                onClick={() => setMenuOpen(false)}
+                            >Inicio</Link>
+                        </li>
+                        <li className='cursor-pointer hover:text-primary' >
+                            <Link
+                                to='programs'
+                                span={true}
+                                smooth={true}
+                                onClick={() => setMenuOpen(false)}
+                            >Programas
+                            </Link>
+                        </li>
+                        <li className='cursor-pointer hover:text-primary' >
+                            <Link
+                                to='features'
+                                span={true}
+                                smooth={true}
+                                onClick={() => setMenuOpen(false)}
+                            >Sobre nosotros
+                            </Link>
+                        </li>
+                        <li className='cursor-pointer hover:text-primary' >
+                            <Link
+                                to='plans'
+                                span={true}
+                                smooth={true}
+                                onClick={() => setMenuOpen(false)}
+                            >Plenes
+                            </Link>
+                        </li>
+                        <li className='cursor-pointer hover:text-primary' >
+                            <Link
+                                onClick={() => setMenuOpen(false)}
+                                to='testimonials'
+                                span={true}
+                                smooth={true}
+                            >Testimonios</Link>
+
+                        </li>
+                    </ul>
+                </div>
+            )}
 
         </div>
     )
--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -15,7 +15,7 @@ function Join() {
     };
 
     return (
-        <div className='mt-24 flex flex-row px-8 gap-40 max-sm:flex-col max-sm:gap-4' id='join'>
+        <div className='mt-24 flex flex-row px-8 gap-40 max-sm:flex-col max-sm:gap-4 max-sm:w-full' id='join'>
             <div className='text-white text-5xl uppercase relative font-bold max-sm:text-2xl max-sm:flex max-sm:flex-col '>
                 <hr className='absolute w-36 border-2 border-primary rounded-[20%] -my-[10px]' />
                 <div className='flex gap-4 mb-2 '>
@@ -27,9 +27,9 @@ function Join() {
                     <span className='stroke-text'> CON NOSOTROS?</span>
                 </div>
             </div>
-            <div className='flex justify-center items-end max-sm:py-8 max-sm:w-10/12 max-sm:self-center'>
-                <form ref={form} onSubmit={sendEmail} className='flex gap-12 bg-slate-600 py-4 px-8
-                max-sm:py-4 max-sm:px-6'>
+            <div className='flex justify-center items-end max-sm:py-8 max-sm:w-9/12 max-sm:self-center'>
+                <form ref={form} onSubmit={sendEmail} className='flex gap-12 max-sm:gap-4 bg-slate-600 py-4 px-8
+                max-sm:py-2 max-sm:px-4'>
                     <input type="email" name='user_email' placeholder='Ingresa tu correo' className='bg-transparent border-none outline-none text-gray-300 placeholder:text-gray-400' />
                     <button className='btn bg-primary text-white max-sm:text-xs'>Únete ahora</button>
                 </form>
